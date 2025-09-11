6a933fda271c4775c451e5e9eede94d38f5bd5f8
Ekaterina Miller <evmiller@nic.edu>
2025-09-11 10:41:43 -0700

    Reverce is done with assumption that file fit in the buffer

--- week4/reverce/main.c
+++ week4/reverce/main.c
@@ -1,3 +1,10 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief Program reverce the lines in the file
+ * @date 2025-09-11
+ * @assumption File is small enough to fit in the buffer
+ */
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -22,24 +29,31 @@ int main(int argc, char * argv[])
         perror("Problem with reading: ");
         exit(1);
     }
-    char * newBuffer= malloc((charRead+1)*sizeof(char));
+    //output will be the same number of characters (no '\n' after last line)
+    char * newBuffer= malloc((charRead)*sizeof(char));
     int end = 0, start = 0;
-    start = end = charRead-1;
+    start = end = charRead-1; //set to the last character
     int index = 0; //write position in new buffer
+
+    //check if file had a \n at the end, if it is, make first line empty
     if (buffer[charRead-1]=='\n'){
         newBuffer[index++] = '\n';
         end--;
         start--;
     }
-    for (int i = end; i>-1; i--){
+    
+    for (int i = end; i>0; i--){
         if (buffer[i]!='\n'){
             start--;
         }else{
-            strncpy(newBuffer+index, buffer+start, end-start+1);
+            strncpy(newBuffer+index, buffer+start+1, end-start);
+            index += end-start;
+            newBuffer[index++]='\n';//put \n at the end of each line
             start = end = i-1;
-            index += end-start+1;
         }
     }
+    //last copy - first line from the buffer
+    strncpy(newBuffer+index, buffer, end+1);
     
     if(write(1, newBuffer, charRead)!=charRead){
         perror(("Problem writing"));
