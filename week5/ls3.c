a8ebd6671722c208a73a76b9d5cd647b61c74d80
Ekaterina Miller <evmiller@nic.edu>
2025-09-17 13:59:25 -0700

    Ls mode is done

--- week5/ls3.c
+++ week5/ls3.c
@@ -2,12 +2,15 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <fcntl.h> 
+#include <sys/sysmacros.h>
 #define True 1
 #define False 0;
 
 void doLs(const char * dirName, int l);
 int isLong(int argc, char *argv[]);
 void showStatInfo(const char *fname, struct stat *buf);
+void getMode(mode_t mode, char * s);
 int main(int argc, char *argv[])
 {
     int l;
@@ -64,6 +67,9 @@ void doLs(const char * dirName, int l)
 void showStatInfo(const char *fname, struct stat *buf)
 {
     printf("   mode: %o\n", buf->st_mode);
+    char mode_str[11]="-----------";
+    getMode(buf->st_mode, mode_str);
+    printf("   mode: %s\n", mode_str);//"-wre--e---e"
     printf("   links: %ld\n", buf->st_nlink);
     printf("   user: %d\n", buf->st_uid);
     printf("   group: %d\n", buf->st_gid);
@@ -80,5 +86,36 @@ int isLong(int argc, char *argv[])
         }
     }
     return False;
+}
+
+void getMode(mode_t mode, char * s)
+{
+    // if (S_ISDIR(mode)) s[0]='b';
+    // else if (S_IS)
+    switch (mode & __S_IFMT) 
+    {
+        case __S_IFBLK:  s[0]='b';        break;
+        case __S_IFCHR:  s[0]='c';        break;
+        case __S_IFDIR:  s[0]='d';        break;
+        case __S_IFIFO:  s[0]='p';        break;
+        case __S_IFLNK:  s[0]='l';        break;
+        case __S_IFSOCK: s[0]='s';        break;
+                //case S_IFREG:  s[0]='-';            break;
+    }
+
+    if (mode & S_IRUSR)
+        s[1]= 'r';
+    if (mode & S_IWUSR)
+        s[2]= 'w';
+    if (mode & S_IXUSR)
+        s[3]= 'x';
+
+    if (mode & S_IRGRP) s[4]= 'r';
+    if (mode & S_IWGRP) s[5]= 'w';
+    if (mode & S_IXGRP) s[6]= 'x';
+    if (mode & S_IROTH) s[7]= 'r';
+    if (mode & S_IWOTH) s[8]= 'w';
+    if (mode & S_IXOTH) s[9]= 'x';
+
 }
 
